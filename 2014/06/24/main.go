38ab8b1237308b7c8d3ea4131826e839dc8a9889
Kawamoto norifumi <mybest.is.next.one@gmail.com>
2017-05-15 23:41:40 +0900

    2014/06

---
+++ 2014/06/24/main.go
@@ -0,0 +1,8 @@
+package main
+
+/**
+https://twitter.com/davecheney/status/481356568240201728
+Dαve Cheney‏ @davecheney  2014年6月24日
+Does this program include any of the std lib apart from the runtime pkg?
+**/
+func main() {}

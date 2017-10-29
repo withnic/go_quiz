e21c1786590eb32015985b292a84969c9e4b85c2
Kawamoto norifumi <mybest.is.next.one@gmail.com>
2017-10-29 17:35:38 +0900

    2017/09/18

---
+++ 2017/09/18/main.go
@@ -0,0 +1,15 @@
+package main
+
+/**
+ * https://twitter.com/codingsince1985/status/909570679695015938
+ * #golang quiz, if
+ * m := make([]int, 10)
+ * which statement below is incorrect
+ *
+ * for _ := range m {
+ * for _ = range m {
+ * for range m {
+ */
+func main() {
+	m := make([]int, 10)
+}
